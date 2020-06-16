b281b2197201bfbf5eb0c95d7901e8e1034ada38
Mike Szalinski <michal@bms.net.au>
2020-06-16 19:44:34 +1000

    Fixed metrics-server on cluster

--- 01-admin/create-cluster.sh
+++ 01-admin/create-cluster.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Clean up screen and terminate any remaining proxy processes
-clear
 kill -9 $(ps -ux | grep 'kubectl proxy' | awk '{print $2}' | head -1)
 
 # delete any cluster by the same name that we are trying to create
@@ -22,10 +21,6 @@ kubectl apply -f ./bind-admin-role.yaml
 BEARER_TOKEN=$(kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | 
 awk '{print $1}') | grep 'token:' | awk '{print $2}')
 
-echo "\n\nCopy the token and paste into the dashboard login page\n"
-echo $BEARER_TOKEN
-echo "\n"
-
 # Add the token to kubernetes config
 kubectl config set-credentials cluster-admin --token=$BEARER_TOKEN
 
@@ -37,17 +32,14 @@ kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceac
 helm init --service-account tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 sleep 20s
-helm install --name metrics-server --namespace kube-system stable/metrics-server --set args="{--kubelet-insecure-tls}"
+helm install --name metrics-server --namespace kube-system stable/metrics-server --set rbac.create=true --set args={"--kubelet-insecure-tls=true, --kubelet-preferred-address-types=InternalIP"}
 sleep 20s
 kubectl apply -f ./kube-state-metrics/examples/standard
-
 
 # Copy the token and copy it into the Dashboard login and press "Sign in"
 kubectl proxy & > /tmp/kube-dashboard.log
 
-
+echo "\n\nCopy the token and paste into the dashboard login page\n"
+echo $BEARER_TOKEN
+echo "\n"
 echo "\n\nEnter the URL on your browser: http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/\n\n"
-
-
-
-
